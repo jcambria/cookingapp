58620c3778a12f4cc1af17b8b8ff706da432ae1c
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-03-28 21:44:30 -0400

    change of direction

--- cookingapp/src/App.js
+++ cookingapp/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Recipe from './Recipe';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>MEALS FOR DADS</h1>
-      <button>View Favorites</button>
-      <Recipe />
+      {/* <h1>MEALS FOR DADS</h1> */}
+      {/* <button>View Favorites</button> */}
+      <Navbar />
+      {/* <Recipe /> */}
     </div>
   );
 }
